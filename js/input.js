f84c3e225a9d0420343aa2993944a5d42947de3e
Jr <jgrantlyons@gmail.com>
2018-12-03 18:27:26 -0800

    first push

---
+++ js/input.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+
+
+    // array to store ingredient for ingredientList table (append a new section of inputs instead)
+    let ingredients = [];
+
+
+    // function to append ingredients to the ingredient list
+    let listNumber = 0;
+
+    $("#addIngredient").on("click", function (event) {
+        event.preventDefault();
+        let newIngredient;
+        let newQuantity;
+        let newUnit;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // beginnings to store data into an array
+
+        // // variables to store user input
+        // let ingredientValue = $("#ingredient").val();
+        // let quantityValue = $("#quantity").val();
+        // let unitValue = $("#unit").val();
+
+
+        // let ingredientObject = {
+        //     ingredient: ingredientValue,
+        //     quantity: quantityValue,
+        //     unit: unitValue
+        // }
+
+        // ingredients.push(ingredientObject);
+
+        // console.log(ingredients);
+
+    });
+
+});
